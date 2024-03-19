176353c2833e5ee15b5b7d24b31588cb2c392760
Shomik Das <dashomik@gamil.com>
2024-03-19 11:47:22 +0530

    Your commit message here

--- Backend/index.js
+++ Backend/index.js
@@ -5,17 +5,21 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 const port = process.env.PORT || 4000;
 
 app.get("/",(req,res)=>{
     res.send("home page");
+})
+
+app.get("/shomik",(req,res)=>{
+  res.send("this is shomik");
 })
 
 const todoRoute = require("./routes/todoRoute")
